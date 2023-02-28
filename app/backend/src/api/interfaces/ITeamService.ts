f8aefaf2c1f597954f079c38922ab6209e4f6dcd
Gabriel Coelho <gabrielcoelhodev1995@gmail.com>
2023-02-28 15:52:31 -0300

    requisito 03 feito

--- app/backend/src/api/interfaces/ITeamService.ts
+++ app/backend/src/api/interfaces/ITeamService.ts
@@ -1,6 +1,5 @@
-import ITeam from './ITeam';
 import TeamModel from '../../database/models/TeamModel';
 
 export default interface ITeamService {
-  findAll(dta: ITeam): Promise<TeamModel[]>,
+  findAll(): Promise<TeamModel[]>,
 }
--- app/backend/src/api/services/TeamService.ts
+++ app/backend/src/api/services/TeamService.ts
@@ -1,6 +1,13 @@
-// import ITeamService from '../interfaces/ITeamService';
-// import {} from '../../database/models/TeamModel';
+import { ModelStatic } from 'sequelize';
+import ITeamService from '../interfaces/ITeamService';
+// import ITeam from '../interfaces/ITeam';
+import TeamModel from '../../database/models/TeamModel';
 
-// export default class TeamService implements ITeamService {
+export default class TeamService implements ITeamService {
+  protected model: ModelStatic<TeamModel> = TeamModel;
 
-// }
+  async findAll(): Promise<TeamModel[]> {
+    const teams = await this.model.findAll();
+    return teams;
+  }
+}
