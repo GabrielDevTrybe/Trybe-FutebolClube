2a8d225ea71e17c9f8bc1ce8c388310d433a3ad6
Gabriel Coelho <gabrielcoelhodev1995@gmail.com>
2023-02-28 15:30:47 -0300

    subindo sem erro de lint

--- app/backend/src/api/services/TeamService.ts
+++ app/backend/src/api/services/TeamService.ts
@@ -1,5 +1,6 @@
-import ITeamService from "../interfaces/ITeamService";
+// import ITeamService from '../interfaces/ITeamService';
+// import {} from '../../database/models/TeamModel';
 
-export default class TeamService implements ITeamService {
+// export default class TeamService implements ITeamService {
 
-}
+// }
